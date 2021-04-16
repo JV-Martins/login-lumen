16825c64915618b5b3bbb6b650ce3c89235cd55d
JV-Martins <jvkm2001@gmail.com>
2021-04-16 12:39:40 -0300

    Primeiro commit

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class UserFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = \App\Models\User::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'email' => $this->faker->unique()->safeEmail,
+            'company' => $this->faker->sentence,
+            'date_nasc' => $this->faker->date,
+            'phone' => $this->faker->sentence,
+        ];
+    }
+}
---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\User::factory()->count(30)->create(); 
+        // factory(\App\Models\User::class,10)->create();
+    }
+}
